218e3f2035bab7928552c1eee58567baf0fd534f
Connor Milliken <cmilliken09@gmail.com>
2018-07-23 10:44:38 -0400

    Update firebase.js
    Removed Key

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyAxKX7bhtdP2nz5H_VBceIbOQiPABRzP_0",
-  authDomain: "product-manager-f215d.firebaseapp.com",
-  databaseURL: "https://product-manager-f215d.firebaseio.com",
-  projectId: "product-manager-f215d",
-  storageBucket: "product-manager-f215d.appspot.com",
-  messagingSenderId: "350683366953"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 firebase.initializeApp(config);
 export default firebase;
